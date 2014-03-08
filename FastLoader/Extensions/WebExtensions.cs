99e6f4e012bcf8a384d918ae082bd46e4c2a761f
Astmus <astmusresist@gmail.com>
2014-03-08 23:38:18 +0200

    Added localization, re worked and upgraded navigation and save history algorithm.

---
+++ FastLoader/Extensions/WebExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastLoader.Extensions
+{
+	public static class WebExtensions
+	{
+		static char[] _invalidChars;
+		static WebExtensions()
+		{
+			_invalidChars = Path.GetInvalidPathChars().Concat(Path.GetInvalidFileNameChars()).ToArray();
+		}
+
+		public static Uri RemoveQueryParams(this Uri uri, params string[] keys)
+		{
+			UriBuilder b = new UriBuilder(uri);
+			int position;
+			Dictionary<string, string> queryParams = null;
+			if ((position = b.Path.IndexOf('&')) != -1)
+			{
+				queryParams = b.Path.Substring(position).Split(new[] { "&" }, StringSplitOptions.RemoveEmptyEntries).Select(par => par.Split('=')).ToDictionary(x => x[0], x => x[1]);
+				b.Path = b.Path.Remove(position);
+
+				foreach (string key in keys)
+					if (queryParams.ContainsKey(key))
+						queryParams.Remove(key);
+
+				b.Query = string.Join("&", queryParams.Select(x => x.Key + "=" + x.Value).ToArray());
+			}
+			return b.Uri;
+		}
+
+		public static string GetLocalHystoryFileName(this Uri uri)
+		{
+			StringBuilder b = new StringBuilder(uri.OriginalString);
+
+			foreach (char c in _invalidChars)
+				b.Replace(c, ' ');
+
+			//b.Remove(0, 11);
+			b.Insert(0, "storagefile");
+			b.Replace(" ", "");
+			b.Replace(".", "");
+			b.Append(".html");
+			return b.Length < 150 ? b.ToString() : b.ToString(0, 150);
+		}
+
+		public static Uri AsLocalHystoryUri(this Uri uri)
+		{
+			StringBuilder b = new StringBuilder(uri.OriginalString);
+
+			foreach (char c in _invalidChars)
+				b.Replace(c, ' ');
+
+			//b.Remove(0, 11);
+			b.Insert(0, "storagefile");
+			b.Replace(" ", "");
+			b.Replace(".", "");
+			b.Append(".html");
+			return new Uri(b.Length < 150 ? b.ToString() : b.ToString(0, 150),UriKind.Relative);
+		}
+
+
+		public static int WordCount(this String str)
+		{
+			return str.Split(new char[] { ' ', '.', '?' },
+							 StringSplitOptions.RemoveEmptyEntries).Length;
+		}
+	}
+}
--- FastLoader/MainPage.xaml.cs
+++ FastLoader/MainPage.xaml.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.IO.IsolatedStorage;
 using System.Threading.Tasks;
 using System.IO.Compression;
+using FastLoader.Extensions;
 
 namespace FastLoader
 {
@@ -34,6 +35,11 @@ namespace FastLoader
 		{
 			return Domain;
 		}
+
+		public bool IsEmpty()
+		{
+			return Domain == String.Empty || Domain == null;
+		}
 	}
 
 	public partial class MainPage : PhoneApplicationPage
@@ -41,30 +47,23 @@ namespace FastLoader
 		const string GOOGLE_SEARCH_DOMAIN = "https://www.google.com/search?q=";
 		// Constructor
 		HttpWebRequest _request;
-		DomainPagesCount _domain;
-		string _currentFileName;
+		string _currentDomain;
+		//string _currentFileName;
+		Uri _currentPage;
 		Stack<Uri> _hystory = new Stack<Uri>();
-		Stack<DomainPagesCount> _domains = new Stack<DomainPagesCount>();
+		//Stack<DomainPagesCount> _domains = new Stack<DomainPagesCount>();
 		Dictionary<Uri, String> _uriFileNames = new Dictionary<Uri, string>();
-		char[] _invalidChars;
 		public MainPage()
 		{
 			InitializeComponent();			
-			//browser.NavigateToString("<html><body style=\"background: #000000\"></body></html>");
-			Uri start = new Uri("storagefilestart.html", UriKind.Relative);
-			_hystory.Push(start);
-			browser.Navigate(start);
-			_invalidChars = Path.GetInvalidPathChars().Concat(Path.GetInvalidFileNameChars()).ToArray();
-			//browser.Navigate(new Uri("http://www.itbox.ua/"));
-			// Sample code to localize the ApplicationBar
-			//BuildLocalizedApplicationBar();
+			_currentPage = new Uri("storagefilestart.html", UriKind.Relative);
+			browser.Navigate(_currentPage);
 		}
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
 			base.OnNavigatedTo(e);
-			Scheduler.Dispatcher.Schedule(() => { LayoutRoot.Children.Remove(placeholder); }, TimeSpan.FromMilliseconds(100));
-			//browser.Navigate(new Uri("http://www.fotomag.com.ua", UriKind.Absolute));
+			Scheduler.Dispatcher.Schedule(() => { LayoutRoot.Children.Remove(placeholder); }, TimeSpan.FromMilliseconds(150));
 		}
 
 		protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
@@ -74,20 +73,11 @@ namespace FastLoader
 			{
 				e.Cancel = true;
 				Uri previousPage = _hystory.Pop();
-				if (browser.Source == previousPage)
-					previousPage = _hystory.Pop();
-				if (_domain.CountPages != 1)
-					_domain.CountPages--;
-				else
-				{
-					DomainPagesCount dpc = _domains.Pop();
-					if (dpc.Domain == _domain.Domain)
-						_domain = _domains.Pop();
-					else
-						_domain = dpc;
-				}
-
-				browser.Navigate(previousPage);
+				if (_hystory.Count != 1)				
+					SetCurrentDomainFromUrl(previousPage);
+				
+				_currentPage = previousPage;
+				browser.Navigate(previousPage.AsLocalHystoryUri());
 			}
 		}
 
@@ -97,14 +87,14 @@ namespace FastLoader
 			{
 				Uri outputUri;
 				string urlAddress = "http://" + (sender as TextBox).Text;
-				if (Uri.TryCreate(urlAddress, UriKind.Absolute, out outputUri) && Uri.IsWellFormedUriString(urlAddress,UriKind.Absolute))
+				if (Uri.TryCreate(urlAddress, UriKind.Absolute, out outputUri) && Uri.IsWellFormedUriString(urlAddress, UriKind.Absolute) && (sender as TextBox).Text.Contains('.'))
 				{
-					PushDomain(outputUri.OriginalString);
+					SetCurrentDomainFromUrl(outputUri);
 					Navigate(outputUri);
 				}
 				else
 				{
-					PushDomain("https://www.google.com");					
+					SetCurrentDomainFromUrl(new Uri("https://www.google.com",UriKind.Absolute));
 					Search((sender as TextBox).Text);
 				}
 			}
@@ -122,7 +112,6 @@ namespace FastLoader
 				Dispatcher.BeginInvoke(() =>
 				{
 					progressBar.IsIndeterminate = false;
-					_domain.CountPages--;
 					MessageBox.Show(AppResources.ExceptionMessage);
 				});
 				return;
@@ -132,7 +121,9 @@ namespace FastLoader
 			if (response.Headers[HttpRequestHeader.ContentEncoding] == "gzip")
 				sourceStream = new GZipStream(sourceStream, CompressionMode.Decompress);
 
-			using (IsolatedStorageFileStream savefilestr = new IsolatedStorageFileStream(_currentFileName, FileMode.Create, FileAccess.Write, IsolatedStorageFile.GetUserStoreForApplication()))
+			string _fileName = _currentPage.GetLocalHystoryFileName();
+
+			using (IsolatedStorageFileStream savefilestr = new IsolatedStorageFileStream(_fileName, FileMode.Create, FileAccess.Write, IsolatedStorageFile.GetUserStoreForApplication()))
 			{
 				sourceStream.CopyTo(savefilestr);
 				savefilestr.Close();
@@ -140,8 +131,9 @@ namespace FastLoader
 
 			Dispatcher.BeginInvoke(() =>
 			{
-				Uri uriForNavigate = new Uri(_currentFileName, UriKind.Relative);
-				_hystory.Push(uriForNavigate);
+				Uri uriForNavigate = new Uri(_fileName, UriKind.Relative);
+				//MessageBox.Show("Navigating to" + Environment.NewLine + uriForNavigate.OriginalString);
+				//_hystory.Push(uriForNavigate);
 				browser.Navigate(uriForNavigate);
 			});
 		}
@@ -149,9 +141,12 @@ namespace FastLoader
 		void Navigate(Uri link)
 		{
 			this.Focus();
-			_currentFileName = GetFileNameFromUri(link);
+			//_currentFileName = GetFileNameFromUri(link);
 			progressBar.IsIndeterminate = true;
-			_domain.CountPages++;
+
+			_hystory.Push(_currentPage);
+			_currentPage = link;
+
 			_request = WebRequest.CreateHttp(link);
 			_request.BeginGetResponse(new AsyncCallback(HandleResponse), null);
 		}
@@ -166,7 +161,6 @@ namespace FastLoader
 		private void browser_Navigated(object sender, NavigationEventArgs e)
 		{
 			progressBar.IsIndeterminate = false;
-			
 		}
 
 		private void browser_Navigating(object sender, NavigatingEventArgs e)
@@ -176,47 +170,35 @@ namespace FastLoader
 				e.Cancel = true;
 				Uri uriForNavigate = e.Uri;
 				//Debug.WriteLine(browser.Source);
-				uriForNavigate = new Uri(_domain + e.Uri.OriginalString, UriKind.Absolute);
 
 				if (e.Uri.OriginalString.Contains("http"))
 				{
-					Uri u = new Uri(e.Uri.OriginalString.Remove(0, e.Uri.OriginalString.IndexOf("http")), UriKind.Absolute);
-					PushDomain(u.Scheme + "://" + u.Host);
+					uriForNavigate = new Uri(e.Uri.OriginalString.Remove(0, e.Uri.OriginalString.IndexOf("http")), UriKind.Absolute);
+					if (_currentDomain.Contains("google"))
+						uriForNavigate = uriForNavigate.RemoveQueryParams("ei", "sa", "ved", "usg");
+					SetCurrentDomainFromUrl(uriForNavigate);
 				}
+				else
+					uriForNavigate = new Uri(_currentDomain + e.Uri.OriginalString, UriKind.Absolute);
+
 				// if it file exists in the storage then load it
-				if (IsolatedStorageFile.GetUserStoreForApplication().FileExists(GetFileNameFromUri(uriForNavigate)))
+				if (IsolatedStorageFile.GetUserStoreForApplication().FileExists(uriForNavigate.GetLocalHystoryFileName()))
 				{
-					_hystory.Push(uriForNavigate);
-					browser.Navigate(uriForNavigate);
+					_hystory.Push(_currentPage);
+					_currentPage = uriForNavigate;
+					browser.Navigate(uriForNavigate.AsLocalHystoryUri());
 					return;
 				}
 
 				Navigate(uriForNavigate);
 			}
 		}
-
-		void PushDomain(string domain)
+		
+		void SetCurrentDomainFromUrl(Uri navigateUrl)
 		{
-			DomainPagesCount pc = new DomainPagesCount(domain);
-			_domain = pc;
-			_domains.Push(pc);
+			_currentDomain = navigateUrl.Scheme + "://" + navigateUrl.Host;
 		}
-
-		string GetFileNameFromUri(Uri uri)
-		{
-			StringBuilder b = new StringBuilder(uri.OriginalString);
-
-			foreach (char c in _invalidChars)
-				b.Replace(c, ' ');
-
-			b.Remove(0, 11);
-			b.Insert(0, "storagefile");
-			b.Replace(" ", "");
-			b.Replace(".", "");
-			b.Append(".html");
-			return b.Length < 150 ? b.ToString() : b.ToString(0,150);
-		}
-
+		
 		// Sample code for building a localized ApplicationBar
 		//private void BuildLocalizedApplicationBar()
 		//{
