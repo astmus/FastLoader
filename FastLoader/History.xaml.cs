53fac9fb9f414725aad3e39f960df1043bcf40f4
Astmus <astmusresist@gmail.com>
2014-04-14 01:15:08 +0300

    added application bar to cache and history page for implement delete items

--- FastLoader/History.xaml.cs
+++ FastLoader/History.xaml.cs
@@ -11,15 +11,19 @@ using FastLoader.DB;
 using System.Diagnostics;
 using FastLoader.Data;
 using FastLoader.Interfaces;
+using System.Collections;
 
 namespace FastLoader
 {
 	public partial class History : PhoneApplicationPage
 	{
 		IWebItem _item;
+		ApplicationBarIconButton select;
+		ApplicationBarIconButton delete;
 		public History()
 		{
 			InitializeComponent();
+			AppBar();
 		}
 
 		private void SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -34,15 +38,68 @@ namespace FastLoader
 					if (history.ItemsSource == null)
 						history.ItemsSource = FSDBManager.Instance.GetSortedItems<HistoryItem>();
 					break;
-			}
-			
-			
+			}				
 		}
 
-		private void history_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+		void AppBar()
 		{
-			
-			int i = 0;
+			select = new ApplicationBarIconButton();
+			select.IconUri = new Uri("Assets/ApplicationBar.Select.png", UriKind.RelativeOrAbsolute);
+			select.Text = "select";
+			select.Click += OnSelectClick;
+
+			delete = new ApplicationBarIconButton();
+			delete.IconUri = new Uri("Assets/ApplicationBar.Delete.png", UriKind.RelativeOrAbsolute);
+			delete.Text = "delete";
+			delete.Click += OnDeleteClick;
+			ApplicationBar.Buttons.Add(select);
+		}
+
+		private void SetupEmailApplicationBar()
+		{
+			ClearApplicationBar();
+
+			if (cache.IsSelectionEnabled)
+			{
+				ApplicationBar.Buttons.Add(delete);
+				UpdateApplicationBar();
+			}
+			else
+			{
+				ApplicationBar.Buttons.Add(select);
+			}
+			ApplicationBar.IsVisible = true;
+		}
+
+		private void UpdateApplicationBar()
+		{
+			if (cache.IsSelectionEnabled)
+			{
+				bool hasSelection = ((cache.SelectedItems != null) && (cache.SelectedItems.Count > 0));
+				delete.IsEnabled = hasSelection;				
+			}
+		}
+
+		void ClearApplicationBar()
+		{
+			while (ApplicationBar.Buttons.Count > 0)
+			{
+				ApplicationBar.Buttons.RemoveAt(0);
+			}
+		}
+
+		void OnDeleteClick(object sender, EventArgs e)
+		{
+			IList source = cache.ItemsSource as IList;
+			while (cache.SelectedItems.Count > 0)
+			{
+				source.Remove((CachedItem)cache.SelectedItems[0]);
+			}
+		}
+
+		void OnSelectClick(object sender, EventArgs e)
+		{
+			cache.IsSelectionEnabled = true;
 		}
 
 		private void Grid_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -57,6 +114,20 @@ namespace FastLoader
 			if (_item != null)
 				(e.Content as MainPage).Navigate(new Uri(_item.Link,UriKind.Absolute));			
 
+		}
+
+		private void history_IsSelectionEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			SetupEmailApplicationBar();
+		}
+
+		private void cache_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (cache.IsSelectionEnabled)
+			{
+				bool hasSelection = ((cache.SelectedItems != null) && (cache.SelectedItems.Count > 0));
+				delete.IsEnabled = hasSelection;				
+			}
 		}
 	}
 }
