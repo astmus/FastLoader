36e4f1b2e090a2d4e4d53926a6b7483691470496
Astmus <astmusresist@gmail.com>
2015-02-25 23:16:17 +0200

    version updated

--- FastLoader/Settings.xaml.cs
+++ FastLoader/Settings.xaml.cs
@@ -17,7 +17,7 @@ namespace FastLoader
 		{
 			this.DataContext = AppSettings.Instance;
 			InitializeComponent();
-			appName.Text += " (v 1.1.8.26)";			
+			appName.Text += " (v 1.2.0.0)";			
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
